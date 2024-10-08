3f09f4e8753fba1236fea6e253a3789479b5926a
kazarin <kazarin.programmer@gmail.com>
2024-10-09 02:43:16 +0300

    DB settings + liquibase

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # appointment-service - Backend для сервиса записи на прием
-
-
+# Перед запуском локально
+1. Создать БД appointment_db схему appointment и прописать креды в application.yml
+```shell
+# Развернуть постгрес в докере
+docker run --name appointment-postgres -p 5432:5432 -e POSTGRES_DB=appointment_db -e POSTGRES_USER=appointment_admin -e POSTGRES_PASSWORD=12345 -d postgres
+```
+```sql
+create schema appointment;
+```
--- src/test/java/com/kazarin/appointment/AppointmentApplicationTests.java
+++ src/test/java/com/kazarin/appointment/AppointmentApplicationTests.java
@@ -1,9 +1,13 @@
 package com.kazarin.appointment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class AppointmentApplicationTests {
 
 	@Test
