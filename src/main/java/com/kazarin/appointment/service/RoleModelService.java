e302bbbc4c81e2462c45b936e6e4f5403b48304d
kazarin <kazarin.programmer@gmail.com>
2024-10-18 12:00:39 +0300

    Mock Tests

--- src/main/java/com/kazarin/appointment/service/RoleModelService.java
+++ src/main/java/com/kazarin/appointment/service/RoleModelService.java
@@ -6,6 +6,7 @@ import com.kazarin.appointment.entity.RoleModelEntity;
 import java.util.List;
 
 public interface RoleModelService {
+
     List<RoleModelDto> findAll();
 
     RoleModelEntity findByName(String role);
